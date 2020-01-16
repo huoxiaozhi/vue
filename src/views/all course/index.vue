7f7f00aeb1f0fab9c6f53f9fa8602e5b04a45cf7
huoxiaozhi <58355649+huoxiaozhi@users.noreply.github.com>
2020-01-16 14:08:34 +0800

    Create index.vue

---
+++ src/views/all course/index.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="grid-content bg-purple app-container">
+    <div class="filter-container app-container">
+      <el-input v-model="courseQuery.name" placeholder="课程名称" class="filter-item" style="width: 200px;"
+        @keyup.enter.native="handleFilter" />
+      <el-select v-model="courseQuery.school" placeholder="开课学院" class="filter-item" style="width: 180px;">
+        <el-option v-for="item in schools" :key="item" :label="item" :value="item" />
+      </el-select>
+      <el-select v-model="courseQuery.course_attribute" placeholder="课程属性" class="filter-item" style="width: 180px;">
+        <el-option v-for="item in courseattributes" :key="item" :label="item" :value="item" />
+      </el-select>
+      <el-button class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+        Search
+      </el-button>
+    </div>
+    <el-table ref="filterTable" v-loading="listLoading" :data="list" element-loading-text="Loading" size="mini" fit
+      highlight-current-row>
+      <el-table-column type="index" label="序号" width="55" align="center">
+      </el-table-column>
+      <el-table-column prop="school" label="开课单位" width="100" align="center">
+      </el-table-column>
+      <el-table-column prop="code" label="课程编码" width="100" align="center">
+      </el-table-column>
+      <el-table-column prop="name" label="课程名称" width="120" align="center">
+      </el-table-column>
+      <el-table-column prop="course_attribute" label="课程属性" width="100" align="center">
+      </el-table-column>
+      <el-table-column prop="subject_major" label="所属学科/专业" width="100" align="center">
+      </el-table-column>
+      <el-table-column label="课时/学分" width="60" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.period }}/{{ scope.row.credit }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="capacity" label="限选" width="60" align="center">
+      </el-table-column>
+      <el-table-column prop="week" label="开课周" width="80" align="center">
+      </el-table-column>
+      <el-table-column prop="scetion" label="星期节次" width="80" align="center">
+      </el-table-column>
+      <el-table-column prop="classroom" label="教室" width="80" align="center">
+      </el-table-column>
+      <el-table-column prop="teaching_mode" label="授课方式" width="80" align="center">
+      </el-table-column>
+      <el-table-column prop="examination_mode" label="考试方式" width="80" align="center">
+      </el-table-column>
+      <el-table-column prop="chief_professor" label="首席教授" width="80" align="center">
+      </el-table-column>
+      <el-table-column prop="lecturer" label="主讲教师" width="80" align="center">
+      </el-table-column>
+      <el-table-column prop="assistant" label="助教" width="80" align="center">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+  import {
+    fetchList
+  } from '@/api/course'
+  export default {
+    data() {
+      return {
+        list: [],
+        courseQuery: {
+          name: null,
+          school: null,
+          course_attribute: null
+        },
+        listLoading: false,
+        coursesLoading: false,
+        selectedlist: [],
+        selectedset: new Set(),
+        courseattributes: [null, '一级学科核心课', '专业核心课', '一级学科普及课', '专业普及课', '一级学科研讨课', '专业研讨课', '公共必修课', '公共选修课', '高级强化课',
+          '实验课', '其他'
+        ],
+        schools: [null, '计算机科学与技术学院', '人工智能学院', '网络空间安全学院', '马克思主义学院', '外语系', '天文与空间科学学院', '数学科学学院', '体育教研室',
+          '公共政策与管理学院', '经济与管理学院', '地球与行星科学学院', '微电子学院', '未来技术学院', '存济医学院', '航空宇航学院', '物理科学学院', '化学科学学院', '资源与环境学院',
+          '生命科学学院', '人文学院', '工程科学学院', '材料科学与光电技术学院', '电子电气与通信工程学院', '国际学院', '创新创业学院', '心理学系', '纳米科学与技术学院', '艺术中心',
+          '核科学与技术学院', '现代农业科学学院', '化学工程学院', '海洋学院'
+        ]
+      }
+    },
+    created() {
+      this.fetchData(this.courseQuery)
+    },
+    methods: {
+      handleFilter() {
+        this.fetchData(this.courseQuery)
+      },
+      fetchData(params) {
+        this.listLoading = true
+        fetchList(params).then(response => {
+          this.list = response.data.list
+          this.listLoading = false
+        })
+      },
+      updateSelect() {
+        //  更新课程选择
+        this.selectedlist = [...this.selectedset]
+        this.changeClassTable()
+      },
+      classtable_indexMethod(index) {
+        return index + 1
+      }
+    }
+  }
+</script>
+
+<style>
+  .app-container {
+    padding: 10px;
+  }
+
+  .filter-container {
+    padding-bottom: 10px;
+  }
+
+  .filter-item {
+    display: inline-block;
+    vertical-align: middle;
+    margin-bottom: 10px;
+  }
+
+</style>
